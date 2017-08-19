2a9a82ac471add56866729f41c24b0d836e8ef02
joaj.freitas <joaj.freitas@gmail.com>
2017-08-19 03:26:29 +0100

    Infer tags from browser bookmarks folder structure

--- bookmarkimporter.py
+++ bookmarkimporter.py
@@ -2,59 +2,62 @@
 
 import html.parser
 import argparse
+import os
+from pprint import pprint
 
-class bookmark:
-    def __init__(self, link, tag):
-        self.link = link
-        self.tag = tag
-
-class bookmarkhtmlparser(html.parser.HTMLParser):
-    def __init__(self, _list):
-        html.parser.HTMLParser.__init__(self)
-        self.list = _list
-        self.data = ""
+class BookmarkHTMLParser(html.parser.HTMLParser):
+    def __init__(self):
+        self.parser = html.parser.HTMLParser.__init__(self)
+        self.bookmarks = {'None': []}
         self.link = ""
+        self.h3 = False
+        self.tag = None
 
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for attr in attrs:
                 if attr[0] == 'href':
                     self.link = attr[1]
+        if tag == 'h3':
+            self.h3 = True
 
     def handle_data(self, data):
-        if data.isspace() == False:
-            self.data = data
+        if self.h3:
+            self.tag = data
+            self.bookmarks[data] = self.bookmarks.get(data) or []
+            self.h3 = False
 
     def handle_endtag(self, tag):
         if tag == 'a':
-            self.list.append( bookmark( link = self.link, tag = self.data ) )
+            self.bookmarks[str(self.tag)].append(self.link) 
 
-
-def main():
+def parse_args():
     parser = argparse.ArgumentParser(description='Import bookmarks from standard html file to rofi-bookmarks.')
     parser.add_argument('inputfile', metavar='htmlfile' )
     parser.add_argument('outputfile', metavar='bookmarkfile')
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    with open(args.outputfile,'r') as f:
-        last = int(f.readlines()[-1].split('.')[0])
-        f.close()
+
+def main(args):
+    if os.path.isfile(args.outputfile):
+        with open(args.outputfile,'r') as f:
+            last = int(f.readlines()[-1].split('.')[0]) + 1
+    else:
+        last = 0 
 
     inf= open(args.inputfile,'r')
     outf = open(args.outputfile,'a')
 
-    listofbookmarks = []
-
-    p = bookmarkhtmlparser(listofbookmarks)
-
-
+    p = BookmarkHTMLParser()
+    
     p.feed(inf.read())
-
-    for i in listofbookmarks:
-        last = last + 1
-        outf.write(str(last) + '. ' + i.link  + ' '+ i.tag.replace(" ",",") + '\n')
+    
+    for k, v in p.bookmarks.items():
+        for link in v:
+            outf.write(f"{str(last)}. {link} {k}\n")
+            last += 1
 
 
 if __name__ == "__main__":
-    main()
+    main(parse_args())
