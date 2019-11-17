33b62ef918cc45c12fcd897120d671c8cb35f9d4
joajfreitas <joaj.freitas@gmail.com>
2019-11-17 13:25:10 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='marcador',
     description='Simple rofi based bookmark manager',
-    version='0.1',
+    version='0.2',
     author="João Freitas",
     author_email="joaj.freitas@gmail.com",
     license="GPLv3",
     url='https://github.com/joajfreitas/marcador',
-    download_url = 'https://github.com/joajfreitas/marcador/archive/v0.1.tar.gz',
+    download_url = 'https://github.com/joajfreitas/marcador/archive/v0.2.tar.gz',
     packages = find_packages(),
     entry_points={
             'console_scripts': [
