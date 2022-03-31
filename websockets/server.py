e7facca3b4659da8aa2b9a5e27a9ab7c01aec95f
joajfreitas <joaj.freitas@gmail.com>
2022-03-31 23:19:42 +0100

    black format

--- websockets/server.py
+++ websockets/server.py
@@ -3,12 +3,15 @@
 import asyncio
 from websockets import serve
 
+
 async def echo(websocket, path):
     async for message in websocket:
         await websocket.send(message)
 
+
 async def main():
     async with serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
+
 
 asyncio.run(main())
