e781051ac253cafe186ff6744fda3ad90f85569f
João Freitas <joaj.freitas@gmail.com>
2023-09-30 17:44:49 +0100

    Add support for config file

--- marcador/server.py
+++ marcador/server.py
@@ -1,7 +1,10 @@
 import click
+import toml
 import json
 import socket
 import logging
+import appdirs
+from pathlib import Path
 
 from .bottle import route, run, request
 
@@ -67,11 +70,46 @@ def marcador_delete(session, url):
     )
 
 
+def load_config_file():
+    config_path = Path(appdirs.user_config_dir(appname="marcador")) / "marcador.toml"
+    if config_path.is_file():
+        with config_path.open() as f:
+            config = toml.loads(f.read())
+
+    else:
+        config = {}
+
+    return config
+
+
+def config_cli_overloads(config, cli):
+    for key, value in cli.items():
+        if value is not None:
+            config[key] = value
+
+    return config
+
+
+def config_defaults(config, defaults):
+    for key, value in defaults.items():
+        if config.get(key) is None:
+            config[key] = value
+
+    return config
+
+
 @click.command()
-@click.option("--hostname", default="0.0.0.0")
-@click.option("--port", type=int, default=6003)
-@click.option("--root", type=str, default="marcador")
+@click.option("--hostname")
+@click.option("--port", type=int)
+@click.option("--root", type=str)
 def server(hostname, port):
+    config = config_defaults(
+        config_cli_overloads(
+            load_config_file(), {"hostname": hostname, "port": port, "root": root}
+        ),
+        {"hostname": "127.0.0.1", "port": 6003, "root": "marcador"},
+    )
+
     session = JsonProxy(get_db_path())
 
     @route("/")
@@ -104,5 +142,5 @@ def server(hostname, port):
         return marcador_delete(session, url).dict()
 
     app = default_app()
-    app.mount(root, app)
-    app.run(host=hostname, port=port)
+    app.mount(config.get("root"), app)
+    app.run(host=config.get("hostname"), port=config.get("port"))
