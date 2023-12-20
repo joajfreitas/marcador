87016295c75e1796228d02456593dd8a507ac2c4
João Freitas <Joao.MC.Freitas@ctw.bmwgroup.com>
2023-12-20 17:44:24 +0000

    backup

--- marcador2/src/lib.rs
+++ marcador2/src/lib.rs
@@ -2,10 +2,104 @@ pub mod models;
 pub mod schema;
 
 use diesel::prelude::*;
+use diesel::{delete, insert_into};
 use dotenvy::dotenv;
 use std::env;
 
 use serde::{Deserialize, Serialize};
+
+use models::Bookmarks;
+
+#[derive(Serialize, Deserialize)]
+pub struct AddParams {
+    pub url: String,
+    pub description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DeleteParams {
+    pub id: i32,
+}
+
+pub trait BookmarkProxy {
+    fn bookmarks(&self) -> Vec<Bookmarks>;
+    fn add(&self, url: &str, description: &str, tags: Vec<String>);
+    fn delete(&self, id: i32);
+}
+
+pub struct LocalProxy {}
+
+impl BookmarkProxy for LocalProxy {
+    fn bookmarks(&self) -> Vec<Bookmarks> {
+        use self::schema::bookmarks::dsl::*;
+        let conn = &mut establish_connection();
+        bookmarks.load(conn).unwrap()
+    }
+
+    fn add(&self, link: &str, desc: &str, _tags: Vec<String>) {
+        use self::schema::bookmarks::dsl::*;
+
+        let conn = &mut establish_connection();
+        insert_into(bookmarks)
+            .values((url.eq(link), description.eq(desc)))
+            .execute(conn)
+            .unwrap();
+    }
+
+    fn delete(&self, identifier: i32) {
+        use self::schema::bookmarks::dsl::*;
+
+        let connection = &mut establish_connection();
+        delete(bookmarks.filter(id.eq(identifier)))
+            .execute(connection)
+            .unwrap();
+    }
+}
+
+pub struct RemoteProxy {
+    list_endpoint: String,
+    add_endpoint: String,
+    delete_endpoint: String,
+}
+
+impl RemoteProxy {
+    pub fn new(url: &str) -> Self {
+        Self {
+            list_endpoint: url.to_string() + "/list",
+            add_endpoint: url.to_string() + "/add",
+            delete_endpoint: url.to_string() + "/delete",
+        }
+    }
+}
+
+impl BookmarkProxy for RemoteProxy {
+    fn bookmarks(&self) -> Vec<Bookmarks> {
+        reqwest::blocking::get(&self.list_endpoint)
+            .unwrap()
+            .json::<Vec<Bookmarks>>()
+            .unwrap()
+    }
+
+    fn add(&self, link: &str, desc: &str, _tags: Vec<String>) {
+        let client = reqwest::blocking::Client::new();
+        client
+            .post(&self.add_endpoint)
+            .json(&AddParams {
+                url: link.to_string(),
+                description: desc.to_string(),
+            })
+            .send()
+            .unwrap();
+    }
+    fn delete(&self, identifier: i32) {
+        let client = reqwest::blocking::Client::new();
+        client
+            .post(&self.delete_endpoint)
+            .json(&DeleteParams { id: identifier })
+            .send()
+            .unwrap();
+    }
+}
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
--- marcador2/src/models.rs
+++ marcador2/src/models.rs
@@ -2,7 +2,7 @@ use diesel::prelude::*;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable, Serialize, Deserialize)]
+#[derive(Queryable, Selectable, Serialize, Deserialize, Debug)]
 #[diesel(table_name = crate::schema::bookmarks)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Bookmarks {
