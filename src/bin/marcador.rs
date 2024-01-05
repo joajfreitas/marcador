2e2cfee325d5af59553cf82e598f0f51c3972356
joajfreitas <joaj.freitas@gmail.com>
2024-01-05 20:38:37 +0000

    Split server into new binary

--- src/bin/marcador.rs
+++ src/bin/marcador.rs
@@ -1,80 +1,28 @@
-use clap::{CommandFactory, Parser, Subcommand};
-use copypasta::{ClipboardContext, ClipboardProvider};
-use itertools::intersperse;
+use clap::{Parser, Subcommand};
 
-use marcador::models::{Bookmarks, Tags};
-use marcador::rofi;
-use marcador::server::server;
+use marcador::rofi_interface::command_rofi;
 use marcador::{BookmarkProxy, LocalProxy, RemoteProxy};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    #[arg(long)]
+    host: Option<String>,
+    #[arg(long)]
+    db: Option<String>,
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    Rofi {
-        #[arg(long)]
-        host: Option<String>,
-        #[arg(long)]
-        db: Option<String>,
-    },
-    Server {
-        db: String,
-    },
+    Rofi,
     Add {
-        #[arg(long)]
-        host: Option<String>,
-        #[arg(long)]
-        db: Option<String>,
         url: String,
         description: String,
         tags: Vec<String>,
     },
-    List {
-        #[arg(long)]
-        host: Option<String>,
-        #[arg(long)]
-        db: Option<String>,
-    },
-}
-
-fn rofi_add(proxy: &dyn BookmarkProxy) -> Result<(), String> {
-    let mut ctx = ClipboardContext::new().map_err(|_| "Failed to create clipboard context")?;
-    let content = ctx
-        .get_contents()
-        .map_err(|_| "Failed to get clipboard contents")?;
-    let s = rofi::Rofi::new(&[content])
-        .prompt("URL")
-        .run()
-        .map_err(|_| "Adding bookmark aborted")?
-        .1
-        .unwrap();
-
-    let v: Vec<String> = vec![];
-    let description = rofi::Rofi::new(&v)
-        .prompt("Description")
-        .run()
-        .map_err(|_| "Adding description aborted")?
-        .1
-        .unwrap();
-    proxy.add(&s, &description, vec![])
-}
-
-fn rofi_delete(
-    proxy: &dyn BookmarkProxy,
-    index: usize,
-    books: Vec<(Bookmarks, Vec<Tags>)>,
-) -> Result<(), String> {
-    proxy.delete(books[index].0.id)
-}
-
-fn rofi_open(url: &str) -> Result<(), String> {
-    open::with(url, "firefox").map_err(|_| "Failed to open url")?;
-    Ok(())
+    List,
 }
 
 fn get_proxy(host: Option<String>, db: Option<String>) -> Result<Box<dyn BookmarkProxy>, String> {
@@ -87,69 +35,24 @@ fn get_proxy(host: Option<String>, db: Option<String>) -> Result<Box<dyn Bookmar
     }
 }
 
-fn command_rofi(proxy: &dyn BookmarkProxy) -> Result<(), String> {
-    let bookmarks = proxy.bookmarks()?;
-
-    let books = bookmarks
-        .iter()
-        .map(|x| x.0.url.to_string())
-        .collect::<Vec<String>>();
-
-    let ret = rofi::Rofi::new(&books)
-        .kb_custom(1, "Alt+n")
-        .kb_custom(2, "Alt+d")
-        .prompt("> ")
-        .message("<b>Alt+n</b>: Add new bookmark <b>Alt+d</b>: Delete bookmark")
-        .run_index();
-
-    match ret {
-        Ok((10, _)) => rofi_add(proxy),
-        Ok((11, Some(index))) => rofi_delete(proxy, index, bookmarks),
-        Ok((0, Some(index))) => rofi_open(&bookmarks[index].0.url),
-        Err(_) => Ok(()),
-        _ => panic!(),
-    }?;
-
-    Ok(())
-}
-
 fn main() -> Result<(), String> {
     let cli = Cli::parse();
-    let mut cmd = Cli::command();
 
-    if let Some(command) = cli.command {
-        match command {
-            Commands::Rofi { host, db } => command_rofi(&*get_proxy(host, db)?),
-            Commands::Server { db } => server(db),
-            Commands::Add {
-                host,
-                db,
-                url,
-                description,
-                tags,
-            } => get_proxy(host, db)?.add(&url, &description, tags),
-            Commands::List { host, db } => {
-                let bookmarks = get_proxy(host, db)?.bookmarks()?;
-                for (bookmark, tags) in &bookmarks {
-                    println!(
-                        "{}. {} - {} [{}]",
-                        bookmark.id,
-                        bookmark.url,
-                        bookmark.description,
-                        intersperse(
-                            tags.iter().map(|tag| format!("{}", tag.tag)),
-                            ", ".to_string()
-                        )
-                        .collect::<String>()
-                    );
-                }
-                Ok(())
+    let proxy = get_proxy(cli.host, cli.db)?;
+    match cli.command {
+        Commands::Rofi => command_rofi(&*proxy),
+        Commands::Add {
+            url,
+            description,
+            tags,
+        } => proxy.add(&url, &description, tags),
+        Commands::List => {
+            for bookmark in proxy.bookmarks()? {
+                println!("{}\n", bookmark);
             }
-        }?;
-    } else {
-        cmd.print_help().unwrap();
-        return Ok(());
-    }
+            Ok(())
+        }
+    }?;
 
     Ok(())
 }
---
+++ src/bookmark.rs
@@ -0,0 +1,41 @@
+use itertools::intersperse;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+use colored::Colorize;
+
+use crate::models::{Bookmarks, Tags};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Bookmark {
+    pub bookmark: Bookmarks,
+    pub tags: Vec<Tags>,
+}
+
+impl Bookmark {
+    pub fn new(bookmark: &Bookmarks, tags: &[Tags]) -> Bookmark {
+        Bookmark {
+            bookmark: bookmark.clone(),
+            tags: tags.to_vec(),
+        }
+    }
+}
+
+impl fmt::Display for Bookmark {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}. {}\n  {} {}\n  {} {}",
+            self.bookmark.id.to_string().cyan(),
+            self.bookmark.description.to_string().green(),
+            ">".red(),
+            self.bookmark.url.to_string().yellow(),
+            "#".red(),
+            intersperse(
+                self.tags.iter().map(|tag| tag.tag.blue().to_string()),
+                ",".blue().to_string()
+            )
+            .collect::<String>()
+        )
+    }
+}
