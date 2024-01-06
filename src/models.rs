80eddd69cf76f23dd45b5ecb10928e80856b02ce
joajfreitas <joaj.freitas@gmail.com>
2024-01-06 17:33:47 +0000

    Update README

--- src/models.rs
+++ src/models.rs
@@ -11,7 +11,6 @@
 // You should have received a copy of the GNU General Public License along with this
 // program. If not, see <https://www.gnu.org/licenses/>.
 
-
 use diesel::prelude::*;
 
 use serde::{Deserialize, Serialize};
