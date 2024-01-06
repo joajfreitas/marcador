a74a5508f29107a0882f1f457a7d65d5e2160857
joajfreitas <joaj.freitas@gmail.com>
2024-01-06 00:08:06 +0000

    Move implementation of binaries into src/

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,37 +1,5 @@
-use clap::Parser;
-use marcador::config::{Config, ServerConfig};
 use marcador::server::server;
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[arg(long)]
-    db: Option<String>,
-    #[arg(long)]
-    host: Option<String>,
-    #[arg(long)]
-    port: Option<u16>,
-    #[arg(long)]
-    root: Option<String>,
-}
-
 fn main() -> Result<(), String> {
-    let cli = Cli::parse();
-
-    let config = Config::read().ok_or("Failed to read config".to_string())?;
-
-    let mut server_config = if let Some(server_config) = config.server {
-        server_config
-    } else {
-        ServerConfig::default()
-    };
-
-    server_config.set_db(&cli.db);
-    server_config.set_host(&cli.host);
-    server_config.set_port(&cli.port);
-    server_config.set_root(&cli.root);
-
-    server(server_config)?;
-
-    Ok(())
+    server()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,17 @@ use diesel::prelude::*;
 use diesel::{delete, insert_into};
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 
+use clap::{Parser, Subcommand};
+
 use dotenvy::dotenv;
 
 use serde::{Deserialize, Serialize};
 
 use bookmark::Bookmark;
 use models::{Bookmarks, Tags};
+
+use crate::config::Config;
+use crate::rofi_interface::command_rofi;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
@@ -210,4 +215,63 @@ pub fn establish_connection(url: &str) -> Result<SqliteConnection, String> {
     dotenv().ok();
 
     SqliteConnection::establish(url).map_err(|_| format!("Error connecting to {}", url))
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long)]
+    host: Option<String>,
+    #[arg(long)]
+    db: Option<String>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Rofi,
+    Add {
+        url: String,
+        description: String,
+        tags: Vec<String>,
+    },
+    List,
+}
+
+fn get_proxy(host: Option<String>, db: Option<String>) -> Result<Box<dyn BookmarkProxy>, String> {
+    if let Some(db) = db {
+        Ok(Box::new(LocalProxy::new(&db)))
+    } else if let Some(host) = host {
+        Ok(Box::new(RemoteProxy::new(&host)))
+    } else {
+        Err("You must provide either a --host or --db flag".to_string())
+    }
+}
+
+pub fn marcador() -> Result<(), String> {
+    let cli = Cli::parse();
+
+    let mut config = Config::read().ok_or("Failed to read config".to_string())?;
+
+    config.set_host(&cli.host);
+    config.set_db(&cli.db);
+
+    let proxy = get_proxy(config.host, config.db)?;
+    match cli.command {
+        Commands::Rofi => command_rofi(&*proxy),
+        Commands::Add {
+            url,
+            description,
+            tags,
+        } => proxy.add(&url, &description, tags),
+        Commands::List => {
+            for bookmark in proxy.bookmarks()? {
+                println!("{}\n", bookmark);
+            }
+            Ok(())
+        }
+    }?;
+
+    Ok(())
 }
